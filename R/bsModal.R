a59e883789f2077604cf5f393c1e229bfe7525a1
bolaka <bolaka.mukherjee@ayata.com>
2014-12-16 13:23:36 +0530

    Adding bootstrap 3 modal

--- R/bsModal.R
+++ R/bsModal.R
@@ -22,6 +22,25 @@ bsModal <- function(id, title, trigger, ..., href) {
   
 }
 
+# Adding a bootstrap 3 modal dialog
+bsModalBoot3 <- function (id, title, trigger, ..., href) 
+{
+  mo <- tags$div(class = "modal sbs-modal fade", id = id, 'data-trigger' = trigger, tabindex="-1", role="dialog", 'aria-labelledby'="myModalLabel", 'aria-hidden'="true",
+                 tags$div(class = "modal-dialog", tags$div(class = "modal-content", tags$div(class = "modal-header", 
+                                                                                             tags$button(type = "button", class = "close", 'data-dismiss' = "modal", 
+                                                                                                         HTML("&times;")), tags$h3(class = "modal-title", id="myModalLabel", title)), body <- tags$div(class = "modal-body"), 
+                                                           tags$div(class = "modal-footer", tags$a(href = "#", class = "btn", 
+                                                                                                   'data-dismiss' = "modal", "Close")))))
+  if (!missing(href)) {
+    mo <- addAttribs(mo, 'data-remote' = href)
+  }
+  else {
+    mo$children[[1]]$children[[1]]$children[[2]] <- tagAppendChildren(mo$children[[1]]$children[[1]]$children[[2]], 
+                                                                      list = list(...))
+  }
+  return(mo)
+}
+
 toggleModal <- function(session, modalId) {
   
   session$sendInputMessage(modalId, list(toggle = TRUE))
